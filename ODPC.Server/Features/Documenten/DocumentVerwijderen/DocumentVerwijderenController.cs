07f8ee0e0894a5026e060dd51ceb7f8e8bc02318
nijmra <armijn@gmail.com>
2025-06-19 15:31:59 +0200

    document delete back-end

---
+++ ODPC.Server/Features/Documenten/DocumentVerwijderen/DocumentVerwijderenController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ODPC.Apis.Odrc;
+using ODPC.Authentication;
+
+namespace ODPC.Features.Documenten.DocumentVerwijderen
+{
+    [ApiController]
+    public class DocumentVerwijderenController(IOdrcClientFactory clientFactory, OdpcUser user) : ControllerBase
+    {
+        const string Concept = "concept";
+        
+        [HttpDelete("api/{version}/documenten/{uuid:guid}")]
+        public async Task<IActionResult> Delete(string version, Guid uuid, CancellationToken token)
+        {
+            using var client = clientFactory.Create("Document verwijderen");
+
+            var url = $"/api/{version}/documenten/{uuid}";
+
+            // document ophalen
+            using var getResponse = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, token);
+
+            if (!getResponse.IsSuccessStatusCode)
+            {
+                return StatusCode(502);
+            }
+
+            var json = await getResponse.Content.ReadFromJsonAsync<PublicatieDocument>(token);
+
+            if (json?.Eigenaar?.identifier != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (json?.Publicatiestatus != Concept)
+            {
+                return StatusCode(502);
+            }
+
+            // document verwijderen
+            using var deleteResponse = await client.DeleteAsync(url, token);
+
+            return !deleteResponse.IsSuccessStatusCode ? StatusCode(502) : StatusCode(204);
+        }
+    }
+}
